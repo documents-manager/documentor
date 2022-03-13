cdb4dc8353cd1a1e663e3eae3a9c4767d74dd7ed
Florian Siepe <flo.siepe@gmail.com>
2022-03-13 18:33:03 +0100

    Staging resource (#60)
    * Added staging resource
    
    * WIP
    
    * Smaller fixes for creating documents
    
    * Fix version
    
    * Improve autocomplete
    
    * Fix some tests
    
    * Make document reference optional for asset
    
    * Bump version
    
    * Fix typo
    
    * Fix test

--- src/main/java/org/documentmanager/boundary/StagingResource.java
+++ src/main/java/org/documentmanager/boundary/StagingResource.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 @Path("staging")
-@Tag(name = "staging")
+@Tag(name = "Staging")
 public class StagingResource {
     @Inject
     AssetService assetService;
