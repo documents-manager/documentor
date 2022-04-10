897d2b3b9a8e1534e461c91a9aea4a757fb32dff
flo.siepe@gmail.com <flo.siepe@gmail.com>
2022-04-10 15:40:30 +0200

    Try to fix test

--- src/test/java/org/documentmanager/control/s3/S3TestFixture.java
+++ src/test/java/org/documentmanager/control/s3/S3TestFixture.java
@@ -1,17 +1,24 @@
 package org.documentmanager.control.s3;
 
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
 import org.documentmanager.entity.s3.FormData;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 
 public class S3TestFixture {
   private S3TestFixture() {}
 
+  @SneakyThrows
   public static FormData createFormData() {
     final var formData = new FormData();
     formData.setFileName("abc.txt");
     formData.setMimeType("application/text");
-    formData.setData(new ByteArrayInputStream("super-cool-content".getBytes()));
+    final var inputStream = new ByteArrayInputStream("super-cool-content".getBytes());
+    final var file = new File("test");
+    FileUtils.copyInputStreamToFile(inputStream, file);
+    formData.setData(file);
     return formData;
   }
 }
